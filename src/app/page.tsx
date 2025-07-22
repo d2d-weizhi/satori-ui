8d34144f80d8ee551016a1ff9efe2538d2ff71a9
d2d.weizhi <d2d.weizhi@gmail.com>
2025-07-22 15:40:22 +0800

    Hide Code Usage in tablet landscape mode and smaller widths

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -290,7 +290,7 @@ export default function Page() {
             </div>
             {/* Code Usage */}
             <div 
-              className="xl:flex xl:w-[380px] lg:flex-1 hidden justify-center items-center h-full p-4 text-gray-50 bg-slate-800 font-mono text-lg rounded-r-xl"
+              className="xl:flex xl:w-[380px] hidden justify-center  items-center h-full p-4 text-gray-50 bg-slate-800 font-mono text-lg rounded-r-xl"
               style={{
                 lineHeight: "1.7rem"
               }}
